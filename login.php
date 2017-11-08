65b70bfc2374053336c11b9184d1dd765320d18f
Marco Oliveira <marcomoa@hotmail.com>
2017-11-08 13:56:03 -0200

    Update and rename login(cast)working.php to login.php (#640)

--- login.php
+++ login.php
@@ -6,44 +6,33 @@
  * Date: 18/09/17
  * Time: 03:01
  */
-
 require 'config/config.php';
-
 // comment to show E_NOTICE [undefinied variable etc.], comment if you want make script and see all errors
 error_reporting(E_ALL ^ E_STRICT ^ E_NOTICE);
-
 // true = show sent queries and SQL queries status/status code/error message
 define('DEBUG_DATABASE', false);
-
 define('INITIALIZED', true);
-
 if (!defined('ONLY_PAGE'))
     define('ONLY_PAGE', true);
-
 // check if site is disabled/requires installation
 include_once('./system/load.loadCheck.php');
-
 // fix user data, load config, enable class auto loader
 include_once('./system/load.init.php');
-
 // DATABASE
 include_once('./system/load.database.php');
 if (DEBUG_DATABASE)
     Website::getDBHandle()->setPrintQueries(true);
 // DATABASE END
-
 /*error example:
 {
     "errorCode":3,
     "errorMessage":"Account name or password is not correct."
 }*/
-
 # Declare variables with array structure
 $characters = array();
 $playerData = array();
 $data = array();
 $isCasting = false;
-
 # error function
 function sendError($msg){
     $ret = array();
@@ -52,22 +41,17 @@ function sendError($msg){
     
     die(json_encode($ret));
 }
-
 # getting infos
 $request = file_get_contents('php://input');
 $result = json_decode($request, true);
-
 # account infos
 $accountName = $result["accountname"];
 $password = $result["password"];
-
 # game port
 $port = 7172;
-
 # check if player wanna see cast list
 if (strtolower($accountName) == "cast")
 	$isCasting = true;
-
 if ($isCasting) {
 	$casts = $SQL->query("SELECT `player_id` FROM `live_casts`")->fetchAll();
 	if (count($casts[0]) == 0)
@@ -81,16 +65,16 @@ if ($isCasting) {
 			$characters[] = $char;
 		}			
 	}
-	
 	$port = 7173;
 	$lastLogin = 0;
+	$premiumAccount = true;
+	$timePremium = 0;
 } else {
 	$account = new Account();
 	$account->find($accountName);
 	
 	if (!$account->isLoaded())
 		sendError("Failed to get account. Try again!");
-
 	if ($account->getPassword() != Website::encryptPassword($password))
 		sendError("The password for this account is wrong. Try again!");
 	
@@ -100,8 +84,9 @@ if ($isCasting) {
 	}
 	
 	$lastLogin = $account->getLastLogin();
+	$premiumAccount = ($account->isPremium()) ? true : false;
+	$timePremium = time() + ($account->getPremDays() * 86400);
 }
-
 $session = array(
 	"fpstracking" => false,
 	"isreturner" => true,
@@ -109,11 +94,10 @@ $session = array(
 	"showrewardnews" => false,
 	"sessionkey" => $accountName . "\n" . $password,
 	"lastlogintime" => $lastLogin,
-    "ispremium" => true,
-    "premiumuntil" => 0,
+    "ispremium" => $premiumAccount,
+    "premiumuntil" => $timePremium,
     "status" => "active"	
 );
-
 $world = array(
 	"id" => 0,
 	"name" => $config['server']['serverName'],
@@ -124,6 +108,7 @@ $world = array(
     "anticheatprotection" => false
 );
 
+//Survey by: Cjaker
 $survey = array(
 	"id": rand(0, 999999),
 	"invitationtext": "Querido tibiano, obrigado por usar OTX, a base mais atualizada do Tibia Global.\n'Mensagem dita por Cjaker'.",
@@ -135,8 +120,8 @@ $survey = array(
 // token=invitationtoken
 // o endtimestamp acima é o tempo convertido em unix timestamp, onde o mesmo é o prazo que irá acabar o survey!
 
-$worlds = array($world);
 
+$worlds = array($world);
 $data["session"] = $session;
 $playerData["worlds"] = $worlds;
 $playerData["characters"] = $characters;
