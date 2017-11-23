af55733068be9069bbfa34f94a490cde05f31d70
vankk <nwtr.otland@hotmail.com>
2017-11-23 18:04:06 -0200

    Fix array (#689)

--- login.php
+++ login.php
@@ -6,33 +6,43 @@
  * Date: 18/09/17
  * Time: 03:01
  */
+
 require 'config/config.php';
+
 // comment to show E_NOTICE [undefinied variable etc.], comment if you want make script and see all errors
 error_reporting(E_ALL ^ E_STRICT ^ E_NOTICE);
+
 // true = show sent queries and SQL queries status/status code/error message
 define('DEBUG_DATABASE', false);
 define('INITIALIZED', true);
+
 if (!defined('ONLY_PAGE'))
     define('ONLY_PAGE', true);
+
 // check if site is disabled/requires installation
 include_once('./system/load.loadCheck.php');
+
 // fix user data, load config, enable class auto loader
 include_once('./system/load.init.php');
+
 // DATABASE
 include_once('./system/load.database.php');
 if (DEBUG_DATABASE)
     Website::getDBHandle()->setPrintQueries(true);
+
 // DATABASE END
 /*error example:
 {
     "errorCode":3,
     "errorMessage":"Account name or password is not correct."
 }*/
+
 # Declare variables with array structure
 $characters = array();
 $playerData = array();
 $data = array();
 $isCasting = false;
+
 # error function
 function sendError($msg){
     $ret = array();
@@ -41,14 +51,17 @@ function sendError($msg){
     
     die(json_encode($ret));
 }
+
 # getting infos
-$request = file_get_contents('php://input');
-$result = json_decode($request, true);
+	$request = file_get_contents('php://input');
+	$result = json_decode($request, true);
+
 # account infos
-$accountName = $result["accountname"];
-$password = $result["password"];
+	$accountName = $result["accountname"];
+	$password = $result["password"];
 # game port
-$port = 7172;
+	$port = 7172;
+
 # check if player wanna see cast list
 if (strtolower($accountName) == "cast")
 	$isCasting = true;
@@ -110,16 +123,15 @@ $world = array(
 
 //Survey by: Cjaker
 $survey = array(
-	"id": rand(0, 999999),
-	"invitationtext": "Querido tibiano, obrigado por usar OTX, a base mais atualizada do Tibia Global.\n'Mensagem dita por Cjaker'.",
-	"invitationtoken": "1751f1beddf001e1d36dee78ace974",
-	"endtimestamp": 1510614000
+    "id" => rand(0, 999999),
+    "invitationtext" => "Querido tibiano, obrigado por usar OTX, a base mais atualizada do Tibia Global.\n'Mensagem dita por Cjaker'.",
+    "invitationtoken" => "1751f1beddf001e1d36dee78ace974",
+    "endtimestamp" => 1510614000
 );
 
 // https://limesurvey.cipsoft.com/index.php/survey/index/sid/527875/lang-en?token=1751f1beddf001e1d36dee78ace974
 // token=invitationtoken
 // o endtimestamp acima é o tempo convertido em unix timestamp, onde o mesmo é o prazo que irá acabar o survey!
-
 
 $worlds = array($world);
 $data["session"] = $session;
